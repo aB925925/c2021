91a4bb8c95082a7c9e03014b60d10d2ecbe8d4d6
aB <3535279905@qq.com>
2021-02-27 19:16:14 +0800

    vsb

--- BubbleSort.c
+++ BubbleSort.c
@@ -15,9 +15,11 @@ int ListLength(PNode head); //计算单链表长度
 void ListAllNode(PNode head); //遍历所有结点
 void ListDestory(PNode head); //销毁单链表
 void List2Array(PNode head, SDataType* datas); //单链表转为数组
-PNode Array2List(PNode head, SDataType* datas, int len); //数组转为单链表
+PNode Array2List(PNode head, SDataType* datas, int len); //数组转为单链
 
 #endif //LIST_H_
+
+
 
 void BubbleSort(PNode head); //冒泡排序
 
